ce03e5fb1fef2f061e5ce769fd37925b4aebdfe1
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-17 16:17:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// Multiply num1 and num2 to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Calculate the remainder of dividing num3 by num4, where the remainder should be 4
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Find the highest number in the set and set it to variable max
+let numbers = [5, 8, 2, 20, 3, 7];
+let max = Math.max(...numbers);
 
 // Write your code here
